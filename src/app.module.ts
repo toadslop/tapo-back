46d49be6200a08aed67386b03416e5587e7e9938
brian heise <bnheise@gmail.com>
2021-10-28 08:29:40 +0900

    added user and auth moduels to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,17 @@ import { AppController } from './app.controller';
 import { AppConfigModule } from './config/app/config.module';
 import { PostgresProviderModule } from './providers/database/postgres/provider.module';
 import { CacheProviderModule } from './providers/cache/provider.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './models/users/users.module';
 
 @Module({
-  imports: [AppConfigModule, PostgresProviderModule, CacheProviderModule],
+  imports: [
+    AppConfigModule,
+    PostgresProviderModule,
+    CacheProviderModule,
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
