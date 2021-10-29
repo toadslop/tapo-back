09af90f1016e7a4c10cdd0736d17940f5b16c979
Brian Heise <brian.heise@liferay.com>
2021-10-30 07:37:07 +0900

    added create method to users controller

--- src/models/users/users.controller.ts
+++ src/models/users/users.controller.ts
@@ -1,10 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private userService: UsersService) {}
+
   @Post()
   async create(@Body() createUserDto: CreateUserDto) {
-    return createUserDto;
+    return this.userService.create(createUserDto);
   }
 }
