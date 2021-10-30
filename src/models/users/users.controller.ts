3d72f503de19ef97eb121807040bb05ef5b666b6
Brian Heise <brian.heise@liferay.com>
2021-10-30 17:32:35 +0900

    implement jwt guard for find all users

--- src/models/users/users.controller.ts
+++ src/models/users/users.controller.ts
@@ -1,10 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { User } from './user.entity';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private userService: UsersService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  async findAll(): Promise<User[]> {
+    return this.userService.findAll();
+  }
 
   @Post()
   async create(@Body() createUserDto: CreateUserDto) {
