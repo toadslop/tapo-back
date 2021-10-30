052389efb1b40052c8b67e1ef23c4612e2df6fd1
Brian Heise <brian.heise@liferay.com>
2021-10-30 16:39:07 +0900

    register jwt module in auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,25 @@
 import { Module } from '@nestjs/common';
+import { JwtModule, JwtModuleAsyncOptions } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { AppConfigModule } from 'src/config/app/config.module';
+import { AppConfigService } from 'src/config/app/config.service';
 import { UsersModule } from 'src/models/users/users.module';
 import { AuthService } from './auth.service';
-import { JwtAuthModule } from './jwt/jwt.module';
 import { LocalStrategy } from './local.strategy';
 
 @Module({
-  imports: [UsersModule, PassportModule, JwtAuthModule],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [AppConfigModule],
+      useFactory: async (appConfigService: AppConfigService) => ({
+        secret: appConfigService.jwtSecret,
+        signOptions: { expiresIn: '60s' },
+      }),
+      inject: [AppConfigService],
+    } as JwtModuleAsyncOptions),
+  ],
   providers: [AuthService, LocalStrategy],
   exports: [AuthService],
 })
