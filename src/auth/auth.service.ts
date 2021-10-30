7a4e831b62c51127780428eb0d0d4fe09be8ffe3
brian heise <bnheise@gmail.com>
2021-10-30 11:55:01 +0900

    implemented basic login functionality

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { User } from 'src/models/users/user.entity';
 import { UsersService } from 'src/models/users/users.service';
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
 
-  async validateUser(username: string, pass: string): Promise<any> {
-    const user = await this.usersService.findOneByEmail(username);
+  async validateUser(email: string, pass: string): Promise<any> {
+    console.log(email, pass);
+    const user = await this.usersService.findOneByEmail(email);
+
+    console.log('IN AUTH SERVICE', user);
     if (user && user.password === pass) {
       const { password, ...result } = user;
       return result;
