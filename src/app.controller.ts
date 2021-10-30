d2d0e306c988bdf03434b8baf65617837c366e18
Brian Heise <brian.heise@liferay.com>
2021-10-30 17:30:46 +0900

    import the local auth guard to app controller for handling login

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthService } from './auth/auth.service';
-import { LocalAuthGuard } from './auth/local-auth.guard';
+import { LocalAuthGuard } from './auth/guards/local-auth.guard';
 
 @Controller()
 export class AppController {
