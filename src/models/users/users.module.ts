6c429073e7f539797eb7caf0d14c9217907dd86e
brian heise <bnheise@gmail.com>
2021-11-03 15:23:38 +0900

    import casl module

--- src/models/users/users.module.ts
+++ src/models/users/users.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CaslModule } from 'src/casl/casl.module';
 import { User } from './user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), CaslModule],
   providers: [UsersService],
   controllers: [UsersController],
   exports: [UsersService],
