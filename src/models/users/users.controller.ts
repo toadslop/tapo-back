0d4b7ba2ad020f43471066b2006fadd0860f6d59
brian heise <bnheise@gmail.com>
2021-11-03 15:26:02 +0900

    import the ability factory for testing

--- src/models/users/users.controller.ts
+++ src/models/users/users.controller.ts
@@ -1,17 +1,34 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  ForbiddenException,
+  Get,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { CaslAbilityFactory } from 'src/casl/casl-ability.factory';
 import { CreateUserDto } from './dtos/create-user.dto';
-import { User } from './user.entity';
+import { Action, User } from './user.entity';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-  constructor(private userService: UsersService) {}
+  constructor(
+    private userService: UsersService,
+    private caslAbilityFactory: CaslAbilityFactory,
+  ) {}
 
   @UseGuards(JwtAuthGuard)
   @Get()
-  async findAll(): Promise<User[]> {
-    return this.userService.findAll();
+  async findAll(@Req() { user }): Promise<User[]> {
+    const ability = this.caslAbilityFactory.createForUser(user as User);
+    if (ability.can(Action.Read, User)) {
+      return this.userService.findAll();
+    } else {
+      throw new ForbiddenException();
+    }
   }
 
   @Post()
